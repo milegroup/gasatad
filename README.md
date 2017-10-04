a39d5ccef402a9f1b88c3b5033c55a23986a8c12
Daniel <dpalonso@esei.uvigo.es>
2017-10-05 00:18:21 +0200

    Cabecera de GNU GPL + modificación del fichero README
    Añadida la cabecera que hace referencia a la licencia GNU General Public
    License. Se ha modificado el fichero "README" y se ha creado un nuevo
    fichero llamado "NOTICE" con la lista de paquetes utilizados en el
    código fuente.

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
-# gasatad
+# GASATaD
 Graphical Application for Statistical Analysis of Tabulated Data
 
+
+What is GASATaD?
+
+GASATaD reads .csv or .xls files and allows users to perform different statistical tests across columns of data. It generates high quality plots and it can export them to pdf, svg, png, tiff or eps files.
+Binaries are available for Microsoft Windows, debian-based GNU Linux platforms and soon for macOS.
+
+For more information, please see https://milegroup.github.io/gasatad/
+
+The source code can be downloaded from:
+https://github.com/milegroup/gasatad
+
+Windows Binary, Debian Package and source code:
+https://milegroup.github.io/gasatad/
--- SignificanceTestInterface.py
+++ SignificanceTestInterface.py
@@ -1,5 +1,22 @@
 # -*- coding: utf-8 -*-
 
+'''
+This file is part of GASATaD.
+
+GASATaD is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+GASATaD is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GASATaD.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
 import wx.lib.scrolledpanel
 from scipy.stats.stats import ks_2samp, ranksums, ttest_ind
 import wx.richtext as rt
