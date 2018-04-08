3b083de1dc35a4b5ce25a9dfed9125e9df30bfe4
Leandro Rodríguez <leandroide@gmail.com>
2018-04-08 15:19:05 +0200

    Check updates in master branch

--- MainFrame.py
+++ MainFrame.py
@@ -866,10 +866,10 @@ class MainFrame ( wx.Frame ):
             remoteVersionFile = "https://raw.githubusercontent.com/milegroup/gasatad/master/docs/programVersions/deb.txt"
 
         elif platform=="darwin" and (os.path.realpath(__file__)=="/Applications/GASATaD.app/Contents/MacOS/MainFrame.py" or os.path.realpath(__file__)=="/Applications/GASATaD.app/Contents/MacOS/MainFrame.pyc"):
-            remoteVersionFile = "https://raw.githubusercontent.com/milegroup/gasatad/gh-pages/programVersions/mac.txt"
+            remoteVersionFile = "https://raw.githubusercontent.com/milegroup/gasatad/master/docs/programVersions/mac.txt"
 
         elif platform=="win32" and argv[0].endswith(".exe"):
-            remoteVersionFile = "https://raw.githubusercontent.com/milegroup/gasatad/gh-pages/programVersions/win.txt"
+            remoteVersionFile = "https://raw.githubusercontent.com/milegroup/gasatad/master/docs/programVersions/win.txt"
 
         elif argv[0].endswith("MainApp.py"):
             # print "# Running GASATaD from source"
