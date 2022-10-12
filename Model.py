48605549218424b07e05470954dc4a091094e37b
MILEGroup <leandroide@gmail.com>
2022-10-12 15:30:40 +0200

    Minor change

--- Model.py
+++ Model.py
@@ -15,6 +15,7 @@ You should have received a copy of the GNU General Public License
 along with GASATaD.  If not, see <http://www.gnu.org/licenses/>.
 """
     
+
 class ProgramState():
     
     def __init__(self):
